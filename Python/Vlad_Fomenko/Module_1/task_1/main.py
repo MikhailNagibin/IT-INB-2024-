da3455cb022e3bb63421571d171249cd478f796c
Tugeton <103279873+Doctor-ot-boga@users.noreply.github.com>
2024-11-14 17:07:54 +0300

    modul test taks 1

--- Python/Vlad_Fomenko/Module_1/task_1/main.py
+++ Python/Vlad_Fomenko/Module_1/task_1/main.py
@@ -4,3 +4,6 @@ hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 MAC = uuid.getnode()
 print(" IP:", ip, "\n", "MAC:", MAC)
+
+for ab in range(100):
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
